d84b252919643c55f9848ce69c5b8251522a0689
T4NK <link.piard@yahoo.fr>
2013-03-22 18:19:02 +0100

    Create Start.bat
    Start actuellement.

---
+++ Start.bat
@@ -0,0 +1,9 @@
+LightCore developed by T4NK
+
+Based on nothing source
+Thanks to Funky-Emu members
+
+Connection to MySQL Pro... [*]
+..............................
+Connection enclanched [*]
+End of Developpement for moment.
